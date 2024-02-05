df4362a33736b42e84465415bdd345b5531201b7
Tanmay3475 <tanmay.mehta@etatvasoft.com>
2024-02-05 15:33:07 +0530

    Add project files.

---
+++ HalloDocWeb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using HalloDocWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HalloDocWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+      
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult forgot()
+        {
+            return View();
+        }
+        public IActionResult submit_request()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
