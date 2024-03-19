6a48d01c634c2bdeb65552d8c5b268be0abab894
Tanmay3475 <tanmay.mehta@etatvasoft.com>
2024-03-19 18:25:06 +0530

    Commit

--- HalloDocWeb/Controllers/CaseController.cs
+++ HalloDocWeb/Controllers/CaseController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using System.IO.Compression;
 
 namespace HalloDocWeb.Controllers
 {
@@ -24,15 +25,46 @@ namespace HalloDocWeb.Controllers
             var req1 = applicationDb.Requestclients.FirstOrDefault(req => req.Requestid == id);
             var reg = applicationDb.Regions.FirstOrDefault(r => r.Regionid == req1.Regionid);
             return View(new ViewCaseViewModel { Address = req1.Street, PhoneNumber = req.Phonenumber, Confirmation = req.Confirmationnumber, DateOfBirth = new DateTime(Convert.ToInt32(req1.Intyear), Convert.ToInt32(req1.Strmonth), Convert.ToInt32(req1.Intdate)), Region = reg.Name, Email = req.Email, LastName = req.Lastname, FirstName = req.Firstname, Room = req1.Address, Symptoms = req1.Notes });
+        } 
+        public IActionResult Orders(int id)
+        {
+            var req=applicationDb.Healthprofessionaltypes.ToList();
+           OrdersViewModel
+                viewModel = new OrdersViewModel { healthprofessionaltypes=req};  
+            return View
+                (viewModel);
+        }
+        [HttpPost]
+        public IActionResult Orders(OrdersViewModel ordersViewModel)
+        {
+            var vendor=applicationDb.Healthprofessionals.FirstOrDefault(m=>(m.Profession==ordersViewModel.profession )&& (m.Vendorname==ordersViewModel.business));
+            var req = new Orderdetail { Businesscontact = ordersViewModel.business_contact, Email = ordersViewModel.business_email, Faxnumber = ordersViewModel.fax_number, Noofrefill = ordersViewModel.no_of_refills,Vendorid=vendor.Vendorid,Prescription=ordersViewModel.prescription };
+         applicationDb.Orderdetails.Add(req);
+            applicationDb.SaveChanges();
+            return RedirectToAction("tabs","AdminStatus");
         }
         public IActionResult ViewUploads(int id)
         {
             var request = applicationDb.Requests.FirstOrDefault(m => m.Requestid == id);
             var files = (from m in applicationDb.Requestwisefiles
-                         where m.Requestid == id
+                         where m.Requestid == id && m.Isdeleted== null
                          select m).ToList();
             DashboardViewModel ds = new DashboardViewModel {FirstName=request.Firstname, requestwisefiles = files, requestid = id, Confirmation = request.Confirmationnumber };
             return View(ds);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var request = applicationDb.Requestwisefiles.FirstOrDefault(m => m.Requestwisefileid == id);
+            var t= new System.Collections.BitArray(1);
+            t[0] = true;
+            request.Isdeleted = t;
+            //var files = (from m in applicationDb.Requestwisefiles
+            //             where m.Requestid == id
+            //             select m).ToList();
+            applicationDb.Requestwisefiles.Update(request);
+            applicationDb.SaveChanges();
+            return RedirectToAction("ViewUploads",new { id=request.Requestid });
         }
         public IActionResult ViewNotes(int id)
         {
@@ -82,6 +114,33 @@ namespace HalloDocWeb.Controllers
             return RedirectToAction("tabs", "AdminStatus");
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DownloadAll(int id)
+        {
+            var checkbox = Request.Form["downloadselect"].ToList();
+            var zipname = id.ToString() + "_" + DateTime.Now + ".zip";
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var item in checkbox)
+                    {
+                        var s = Int32.Parse(item);
+                        var file = await applicationDb.Requestwisefiles.FirstOrDefaultAsync(x => x.Requestwisefileid == s);
+                        var path = file.Filename;
+                        var bytes = await System.IO.File.ReadAllBytesAsync(path);
+                        var zipEntry = zipArchive.CreateEntry(file.Filename.Split("\\Documents\\")[1], CompressionLevel.Fastest);
+                        using (var zipStream = zipEntry.Open())
+                        {
+                            await zipStream.WriteAsync(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+                memoryStream.Position = 0; // Reset the position
+                return File(memoryStream.ToArray(), "application/zip", zipname, enableRangeProcessing: true);
+            }
+        }
+        [HttpPost]
         public IActionResult BlockCase(BlockCaseViewModel Vw)
         {
             var req = applicationDb.Requests.FirstOrDefault(req => req.Requestid == Vw.Id);
@@ -93,6 +152,32 @@ namespace HalloDocWeb.Controllers
             applicationDb.Requeststatuslogs.Add(req1);
             applicationDb.SaveChanges();
             return RedirectToAction("tabs", "AdminStatus");
+        }
+        [HttpPost]
+        public IActionResult ClearCase(int id)
+        {
+            var req = applicationDb.Requests.FirstOrDefault(req => req.Requestid == id);
+            req.Status = 11;
+            req.Modifieddate = DateTime.Now;
+            applicationDb.Requests.Update(req);
+           
+            var req1 = new Requeststatuslog { Requestid = req.Requestid, Status = 10, Createddate = DateTime.Now };
+            applicationDb.Requeststatuslogs.Add(req1);
+            applicationDb.SaveChanges();
+            return RedirectToAction("tabs", "AdminStatus");
+        }
+
+        public IActionResult Download(int id)
+        {
+            var path = (applicationDb.Requestwisefiles.FirstOrDefault(x => x.Requestwisefileid == id)).Filename;
+            //var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "document", filename);
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(path, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, contentType, Path.GetFileName(path));
         }
         [HttpPost]
         public IActionResult AssignCase(AssignCaseViewModel Vw)
@@ -112,18 +197,18 @@ namespace HalloDocWeb.Controllers
            var DataTableViewModels=from physician in applicationDb.Physicians where physician.Regionid== regionId select new DataViewModel { First=physician.Firstname,Last=physician.Lastname,id=physician.Physicianid};
             return DataTableViewModels.ToList();
         }
-        public IActionResult Download(int id)
-        {
-            var path = (applicationDb.Requestwisefiles.FirstOrDefault(x => x.Requestwisefileid == id)).Filename;
-            //var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "document", filename);
-            var provider = new FileExtensionContentTypeProvider();
-            if (!provider.TryGetContentType(path, out var contentType))
-            {
-                contentType = "application/octet-stream";
-            }
-            var bytes = System.IO.File.ReadAllBytes(path);
-            return File(bytes, contentType, Path.GetFileName(path));
-        }
+        //public IActionResult Download(int id)
+        //{
+        //    var path = (applicationDb.Requestwisefiles.FirstOrDefault(x => x.Requestwisefileid == id)).Filename;
+        //    //var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "document", filename);
+        //    var provider = new FileExtensionContentTypeProvider();
+        //    if (!provider.TryGetContentType(path, out var contentType))
+        //    {
+        //        contentType = "application/octet-stream";
+        //    }
+        //    var bytes = System.IO.File.ReadAllBytes(path);
+        //    return File(bytes, contentType, Path.GetFileName(path));
+        //}
         public void uploadFile(List<IFormFile> file, int id)
         {
 
@@ -147,6 +232,23 @@ namespace HalloDocWeb.Controllers
             }
 
 
+        }
+        public IActionResult DeleteAll(List<int> check,int id)
+        {
+            foreach(var item in check)
+            {
+                var request = applicationDb.Requestwisefiles.FirstOrDefault(m => m.Requestwisefileid == item);
+                var t = new System.Collections.BitArray(1);
+                t[0] = true;
+                request.Isdeleted = t;
+                //var files = (from m in applicationDb.Requestwisefiles
+                //             where m.Requestid == id
+                //             select m).ToList();
+                applicationDb.Requestwisefiles.Update(request);
+                applicationDb.SaveChanges();
+              
+            }
+            return RedirectToAction("ViewUploads", new { id = id });
         }
         [HttpPost]
         public IActionResult ViewUploads(int id,List<IFormFile> files)
---
+++ Models/OrdersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloDoc.Models
+{
+    public class OrdersViewModel
+    {
+       public List<Healthprofessionaltype> healthprofessionaltypes {  get; set; }
+        public int profession      {  get; set; }
+        public string business         {  get; set; }
+        public string business_contact { get; set;}
+        public string business_email   { get; set;}
+        public string prescription     { get; set;}
+        public string fax_number      { get; set;}
+        public int    no_of_refills   { get; set;}
+
+    }
+}
