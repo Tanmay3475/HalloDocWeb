74a71e2ecd341c168c10189b4f54e2d14ccb5a36
Tanmay3475 <tanmay.mehta@etatvasoft.com>
2024-03-29 18:50:32 +0530

    Commit

---
+++ HalloDocWeb/Controllers/ProviderController.cs
@@ -0,0 +1,25 @@
+﻿using HalloDoc.Models;
+using HalloDoc.Models.DataContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocWeb.Controllers
+{
+    public class ProviderController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProviderController(ApplicationDbContext application)
+        {
+            context = application;
+        }
+        public IActionResult Information()
+        {
+            var req = (from pro in context.Physicians join not in context.Physiciannotifications on pro.Physicianid equals not.Physicianid select new ProviderInfoViewModel { ProviderName = pro.Firstname + ',' + pro.Lastname, Status = (int)pro.Status, BitArray = not.Isnotificationstopped ,id=pro.Physicianid}).ToList();
+            return View(req);
+        }
+        public IActionResult Edit(int Id)
+        {
+        //    var req = (from pro in context.Physicians join not in context.Physiciannotifications on pro.Physicianid equals not.Physicianid select new ProviderInfoViewModel { ProviderName = pro.Firstname + ',' + pro.Lastname, Status = (int)pro.Status, BitArray = not.Isnotificationstopped ,id=pro.Physicianid}).ToList();
+            return View();
+        }
+    }
+}
