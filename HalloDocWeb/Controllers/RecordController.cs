e59dc9672847249d6c7c9f7cc0aac0e71032bf08
Tanmay3475 <tanmay.mehta@etatvasoft.com>
2024-04-23 18:33:46 +0530

    Commit

---
+++ HalloDocWeb/Controllers/RecordController.cs
@@ -0,0 +1,30 @@
+﻿using HalloDoc.Models;
+using HalloDoc.Models.DataContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDocWeb.Controllers
+{
+    public class RecordController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RecordController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult PatientRecord()
+        {
+            return View();
+        }
+        public IActionResult RecordPartial()
+        {
+            var req=_context.Users.ToList();
+            var req1 = new List<RecordViewModel>();
+            foreach(var record in req)
+            {
+                var req2 = new RecordViewModel { FirstName = record.Firstname, LastName = record.Lastname, Email = record.Email, Phone = record.Mobile, Address = record.Street };
+req1.Add(req2);
+            }
+            return View(req1);
+        }
+    }
+}
